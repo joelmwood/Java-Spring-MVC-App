69b018a427e9c68c255fcae8a16d4983f4ccece6
Joel Wood <joelmwood@live.com>
2017-02-15 09:46:35 -0600

    Add springapp Test Class

---
+++ SpringApp/test/springapp/web/HelloControllerTests.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package springapp.web;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import springapp.web.HelloController;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Wood
+ *
+ */
+public class HelloControllerTests extends TestCase{
+	public void testHandleRequestView() throw Exception{
+		HelloController controller = new HelloController();
+		ModelAndView modelAndView = controller.handleRequest(null, null);
+		assertEquals("hello.jsp", modelAndView.getViewName());
+	}
+}
